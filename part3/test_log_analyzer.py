5b1c927d143e2453596dc548efea284ad6768f9a
tp6hannah <hannah51125@gmail.com>
2021-01-24 15:04:02 +0800

    chapter8: Refactor test_semamtic_change by using a utility factory method to return a LogAnalyzer instance

--- part3/test_log_analyzer.py
+++ part3/test_log_analyzer.py
@@ -4,7 +4,12 @@ from part3.log_analyzer import LogAnalyzer
 class TestLogAnalyzer:
 
     def test_semantics_change(self):
-        logan = LogAnalyzer()
-        logan.initialize()
+        logan = make_default_analyzer()
 
         assert logan.is_valid("abc") is False
+
+
+def make_default_analyzer():
+    logan = LogAnalyzer()
+    logan.initialize()
+    return logan

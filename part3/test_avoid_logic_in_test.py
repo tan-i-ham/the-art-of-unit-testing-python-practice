5822a1fbb5da62c21cc885969a989d6f53000320
tp6hannah <hannah51125@gmail.com>
2021-01-24 15:12:45 +0800

    chapter8: Refactoring -> assert with hardcoded value

--- part3/test_avoid_logic_in_test.py
+++ part3/test_avoid_logic_in_test.py
@@ -2,14 +2,12 @@ class MessageBuilder:
 
     @staticmethod
     def build(user: str, greeting_msg: str):
-        return user + greeting_msg
+        return user + " " + greeting_msg
 
 
 class TestAvoidLogic:
 
     def test_production_logic_problem(self):
-        user: str = "USER"
-        greeting: str = "GREETING"
-        actual: str = MessageBuilder.build(user, greeting)
+        actual: str = MessageBuilder.build("USER", "GREETING")
 
-        assert user + greeting == actual
+        assert "USER GREETING" == actual
